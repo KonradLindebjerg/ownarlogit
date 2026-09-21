2f56681106e58f8fe0467a951040cbaf3d18f66f
Konrad <konradlindebjerg@gmail.com>
2026-09-21 15:44:43 +0200

    push

--- landmark2.py
+++ landmark2.py
@@ -70,7 +70,6 @@ def travelLandmark(tvec):
         print("Moving right!")
         arlo.go_diff(leftSpeed, rightSpeed, 1, 0)
         sleep(0.1)
-        arlo.go_diff(leftSpeed, rightSpeed, 0, 1)
         print(arlo.stop())
         sleep(1)
         
@@ -78,7 +77,6 @@ def travelLandmark(tvec):
         print("Moving left!")
         arlo.go_diff(leftSpeed, rightSpeed, 0, 1)
         sleep(0.1)
-        arlo.go_diff(leftSpeed, rightSpeed, 1, 0)
         print(arlo.stop())
         sleep(1)
        
