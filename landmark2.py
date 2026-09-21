7bc85d745d547dd4e38bfb9a1d4886bfa87b4c6a
Yosef <Yosef234@outlook.dk>
2026-09-21 15:36:14 +0200

    push

--- landmark2.py
+++ landmark2.py
@@ -68,17 +68,21 @@ def travelLandmark(tvec):
 
     if x > tolerance:
         print("Moving right!")
-        arlo.go_diff(leftSpeed, rightSpeed, 1, 0)
+        arlo.go_diff(leftSpeed*0.5, rightSpeed0.5, 1, 0)
         sleep(0.2)
+        arlo.stop()
         
     elif x < -tolerance:
         print("Moving left!")
-        arlo.go_diff(leftSpeed, rightSpeed, 0, 1)
+        arlo.go_diff(leftSpeed*0.5, rightSpeed*0.5, 0, 1)
         sleep(0.2)
+        arlo.stop()
        
     else: 
         print("Centered!!")
         arlo.go_diff(leftSpeed, rightSpeed, 1, 1) 
+        sleep(0.2)
+        arlo.stop()
 
 # Estimate the distance from robot to observed landmark
 def estimateLandmark(corners):
